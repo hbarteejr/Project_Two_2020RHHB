e37be6e0d0d428fb973c5e5767f5111f1c8c7031
Howard Bartee, Jr <60280558+hbarteejr@users.noreply.github.com>
2020-06-01 20:56:39 -0500

    Add files via upload

---
+++ NFLDRAFTPICKS20092019.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS round;
+
+
+
+CREATE TABLE round (
+  draft year character varying(45) NOT NULL,
+  round character varying(45) NOT NULL,
+  pick character varying (45) NOT NULL, 
+  player character varying(45) NOT NULL,
+  position character varying(45) NOT NULL,
+  college character varying (45) NOT NULL,
+  conference character varying(45) NOT NULL
+);
